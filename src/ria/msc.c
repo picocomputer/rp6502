002aed98702df06fc614f6f98ed2145c91eb8954
Rumbledethumps <16963588+Rumbledethumps@users.noreply.github.com>
2023-01-05 14:22:49 -0800

    update to tinyusb submodule

--- src/ria/msc.c
+++ src/ria/msc.c
@@ -8,13 +8,17 @@
 
 static scsi_inquiry_resp_t inquiry_resp;
 
-bool inquiry_complete_cb(uint8_t dev_addr, msc_cbw_t const *cbw, msc_csw_t const *csw)
+bool inquiry_complete_cb(uint8_t dev_addr, tuh_msc_complete_data_t const *cb_data)
 {
+    msc_cbw_t const *cbw = cb_data->cbw;
+    msc_csw_t const *csw = cb_data->csw;
+
     if (csw->status != 0)
     {
-        printf("tuh_msc_inquiry failed, status = %d\n", csw->status);
+        printf("USB mass storage device inquiry failed\n");
         return false;
     }
+
     printf("%.8s %.16s rev %.4s\r\n", inquiry_resp.vendor_id, inquiry_resp.product_id, inquiry_resp.product_rev);
     uint32_t const block_count = tuh_msc_get_block_count(dev_addr, cbw->lun);
     uint32_t const block_size = tuh_msc_get_block_size(dev_addr, cbw->lun);
@@ -25,7 +29,9 @@ bool inquiry_complete_cb(uint8_t dev_addr, msc_cbw_t const *cbw, msc_csw_t const
 void tuh_msc_mount_cb(uint8_t dev_addr)
 {
     printf("USB mass storage device mount\n");
-    tuh_msc_inquiry(dev_addr, 0, &inquiry_resp, inquiry_complete_cb);
+
+    uint8_t const lun = 0;
+    tuh_msc_inquiry(dev_addr, lun, &inquiry_resp, inquiry_complete_cb, 0);
 }
 
 void tuh_msc_umount_cb(uint8_t dev_addr)
