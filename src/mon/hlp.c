42bdeaa74c0f343b938c3f6e4b13515f510c9fe9
Rumbledethumps <16963588+Rumbledethumps@users.noreply.github.com>
2023-01-28 11:53:00 -0800

    adding help text

--- src/mon/hlp.c
+++ src/mon/hlp.c
@@ -33,8 +33,82 @@ static const char __in_flash("helptext") hlp_text_help[] =
     "BINARY addr len crc - Write memory. Binary data follows.\n"
     "0000 (00 00 ...)    - Read or write memory.\n";
 
+static const char __in_flash("helptext") hlp_text_about[] =
+    "//TODO Credits will go here.";
+
+static const char __in_flash("helptext") hlp_text_system[] =
+    "This is the RIA monitor of the Picocomputer, not an operating system CLI.\n"
+    "The Picocomputer does not use a traditional parallel ROM like a 27C64 or\n"
+    "similar. Instead, this monitor is used to prepare the 6502 RAM with software\n"
+    "that would normally be on a ROM chip. //TODO more to come.";
+
 static const char __in_flash("helptext") hlp_text_status[] =
-    "//TODO";
+    "STATUS will list all configurable settings and some system information\n"
+    "including a list of USB devices and their ID. The USB ID is also the drive\n"
+    "number for mass storage devices (MSC). Up to 8 devices are supported.";
+
+static const char __in_flash("helptext") hlp_text_caps[] =
+    "CAPS is intended for software that doesn't recognize lower case, like many\n"
+    "versions of BASIC. This is only in effect while 6502 software is running.\n"
+    "It will translate both USB keyboards and the serial console. Setting is\n"
+    "saved on the RIA flash.\n"
+    "  0 = Normal.\n"
+    "  1 = Inverted. Uppercase is lowered. Lowercase is uppered.\n"
+    "  2 = Forced. Lowercase is uppered. Everything is uppercase always.";
+
+static const char __in_flash("helptext") hlp_text_phi2[] =
+    "PHI2 is the 6502 clock speed in kHz. The valid range is 1-8000 but not all\n"
+    "frequencies are available. In that case, the next highest frequency will\n"
+    "be automatically calculated and selected. Faster than the default speed of\n"
+    "4000 kHz (4 MHz) requires \"Advanced CMOS\" glue logic (74ACxx) as well as\n"
+    "overclocking of the Pi Pico RIA, which is handled automatically. The Pi Pico\n"
+    "RIA will run at 240MHz for 8MHz, which is much lower than the 400+ MHz that\n"
+    "it is capable of. Setting is saved on the RIA flash.";
+
+static const char __in_flash("helptext") hlp_text_resb[] =
+    "RESB is the minimum time in milliseconds that the RESB line is held low for\n"
+    "a 6502 reset. This should be left on auto (0) unless you have installed extra\n"
+    "hardware that needs more than 2 cycles of PHI2 while in reset. The range is\n"
+    "0-255ms but beware that large delays will affect loading ROMs. Setting is\n"
+    "saved on the RIA flash.";
+
+static const char __in_flash("helptext") hlp_text_dir[] =
+    "LS (also aliased as DIR) and CD are used to navigate USB mass storage\n"
+    "devices. You can change to a different USB device with 1: to 8:. Use the\n"
+    "STATUS command to get a list of mounted drives.";
+
+static const char __in_flash("helptext") hlp_text_load[] =
+    "LOAD and INFO read ROM files from a USB drive. A ROM file contains both\n"
+    "ASCII information for the user, and binary information for the RP6502.\n"
+    "Lines may end with either LF or CRLF. The first line must be:\n"
+    "#!RP6502\n"
+    "This is followed by HELP/INFO lines that begin with a # and a space:\n"
+    "# Cool Game V0.0 by Awesome Dev\n"
+    "After the info lines, binary data is prefixed with ASCII lines containing\n"
+    "hex or decimal numbers indicating the address, length, and CRC-32.\n"
+    "$C000 1024 0x0C0FFEE0\n"
+    "This is followed by the binary data. The maximum length is 1024 bytes, so\n"
+    "repeat as necessary. The CRC-32 is calculated using the same method as zip.\n"
+    "If the ROM file contains data for the reset vector $FFFC-$FFFD then the\n"
+    "6502 will be reset (started) immediately after loading.";
+
+static const char __in_flash("helptext") hlp_text_install[] =
+    "INSTALL and REMOVE manage the ROMs installed in the Pi Pico RIA flash memory.\n"
+    "ROM files must contain a reset vector to be installed. A list of installed\n"
+    "ROMs is shown on the base HELP screen. Once installed, these ROMs become an\n"
+    "integrated part of the system and can be loaded manually by simply using their\n"
+    "name like any other command. The ROM name must not conflict with any other\n"
+    "system command and may only contain up to 16 ASCII letters. If the file\n"
+    "contains an extension, it must be \".rp6502\", which will be stripped upon\n"
+    "install.";
+
+static const char __in_flash("helptext") hlp_text_boot[] =
+    "BOOT selects an installed ROM to be automatically loaded and started when the\n"
+    "system is power up or rebooted. For example, you might want the system to\n"
+    "immediately boot into BASIC or an operating system CLI. This is used to\n"
+    "provide the instant-on experience of classic 8-bit computers. Using \"-\" for\n"
+    "the argument will have the system boot into the monitor you are using now.\n"
+    "Setting is saved on the RIA flash.";
 
 static struct
 {
@@ -43,6 +117,32 @@ static struct
     const char *const text;
 } const COMMANDS[] = {
     {6, "status", hlp_text_status},
+    {5, "about", hlp_text_about},
+    {6, "system", hlp_text_system},
+    {1, "0", hlp_text_system},
+    {4, "0000", hlp_text_system},
+    {4, "caps", hlp_text_caps},
+    {4, "phi2", hlp_text_phi2},
+    {4, "resb", hlp_text_resb},
+    {2, "ls", hlp_text_dir},
+    {3, "dir", hlp_text_dir},
+    {2, "cd", hlp_text_dir},
+    {2, "0:", hlp_text_dir},
+    {2, "1:", hlp_text_dir},
+    {2, "2:", hlp_text_dir},
+    {2, "3:", hlp_text_dir},
+    {2, "4:", hlp_text_dir},
+    {2, "5:", hlp_text_dir},
+    {2, "6:", hlp_text_dir},
+    {2, "7:", hlp_text_dir},
+    {2, "8:", hlp_text_dir},
+    {2, "9:", hlp_text_dir},
+    {4, "load", hlp_text_load},
+    {4, "info", hlp_text_load},
+    {7, "install", hlp_text_install},
+    {6, "remove", hlp_text_install},
+    {4, "boot", hlp_text_boot},
+
 };
 static const size_t COMMANDS_COUNT = sizeof COMMANDS / sizeof *COMMANDS;
 
