cc81538e37e30fee34e848159924f2effd4578a2
rumbledethumps <16963588+rumbledethumps@users.noreply.github.com>
2023-05-17 15:21:33 -0700

    add audio waveforms

--- src/dev/aud.c
+++ src/dev/aud.c
@@ -12,55 +12,147 @@
 #include "hardware/irq.h"
 #include <math.h>
 #include <stdio.h>
+#include <string.h>
 
-#define AUD_L_PIN 28
-#define AUD_L_CHAN PWM_CHAN_A
+#define AUD_RATE 24000
+#define AUD_BITS 8
+#define AUD_CHANNELS 8
+
+#define AUD_L_CHAN (pwm_gpio_to_channel(AUD_L_PIN))
 #define AUD_L_SLICE (pwm_gpio_to_slice_num(AUD_L_PIN))
-#define AUD_R_PIN 27
-#define AUD_R_CHAN PWM_CHAN_B
+#define AUD_R_CHAN (pwm_gpio_to_channel(AUD_R_PIN))
 #define AUD_R_SLICE (pwm_gpio_to_slice_num(AUD_R_PIN))
-
-#define AUD_IRQ_PIN 14
 #define AUD_IRQ_SLICE (pwm_gpio_to_slice_num(AUD_IRQ_PIN))
 
-#define AUD_PWM_WRAP 255
-#define AUD_RATE 12000
+#define AUD_PWM_WRAP ((1u << AUD_BITS) - 1)
+#define AUD_PWM_CENTER (1u << (AUD_BITS - 1))
+#define AUD_SHIFT (1 + 14 - AUD_BITS)
 
-// Fixed point range of -1.999 to 1.999 for DSP work
+// Fixed point range of -1.9r to 1.9r for DSP work
 typedef signed short s1x14;
 #define muls1x14(a, b) ((s1x14)((((int)(a)) * ((int)(b))) >> 14))
-#define divs1x14(a, b) ((s1x14)((((signed int)(a) << 14) / (b))))
-#define int_to_s1x14(a) ((s1x14)((a) << 14))
 #define float_to_s1x14(a) ((s1x14)((a)*16384.0))
 #define s1x14_to_float(a) ((float)(a) / 16384.0)
+#define s1x14_0_0 (0)
+#define s1x14_1_0 ((s1x14)(1 << 14))
+#define s1x14_1_9r ((s1x14)(1 << 15) - 1)
 
-static s1x14 nco_r, nco_i;
-static s1x14 clk_r, clk_i;
+enum waveform
+{
+    square,
+    sine,
+    // All above use sine, below is linear
+    saw,
+    triangle,
+};
 
-static void __isr __time_critical_func(audio_pwm_dma_irq_handler)()
+struct channel
+{
+    s1x14 nco_r;
+    s1x14 nco_i;
+    s1x14 clk_r;
+    s1x14 clk_i;
+    enum waveform wave;
+    bool dirty;
+};
+
+// Recomputing clocks is too much for an ISR, so it's done as a task
+// then moved from pending[] to chan[] as needed by the ISR.
+static struct channel chan[AUD_CHANNELS];
+static struct channel pending[AUD_CHANNELS];
+
+static void __isr __time_critical_func(audio_pwm_irq_handler)()
 {
     pwm_clear_irq(AUD_IRQ_SLICE);
 
-    s1x14 r = ((int)nco_r * (int)clk_r - (int)nco_i * (int)clk_i) >> 14;
-    s1x14 i = ((int)nco_i * (int)clk_r + (int)nco_r * (int)clk_i) >> 14;
-
-    if (r < 0)
-        pwm_set_chan_level(AUD_L_SLICE, AUD_L_CHAN, 0x80 - (-r >> 7));
-    else
-        pwm_set_chan_level(AUD_L_SLICE, AUD_L_CHAN, 0x80 + (r >> 7));
-
-    static unsigned norm = 1;
-    if (--norm)
+    for (unsigned idx = 0; idx < AUD_CHANNELS; idx++)
     {
-        nco_r = r;
-        nco_i = i;
+        struct channel *this = &chan[idx];
+        enum waveform wave = this->wave;
+
+        if (pending[idx].dirty)
+        {
+            pending[idx].dirty = false;
+            memcpy(this, &pending[idx], sizeof(struct channel));
+        }
+
+        if (wave <= sine)
+        {
+            s1x14 r = ((int)this->nco_r * (int)this->clk_r - (int)this->nco_i * (int)this->clk_i) >> 14;
+            s1x14 i = ((int)this->nco_i * (int)this->clk_r + (int)this->nco_r * (int)this->clk_i) >> 14;
+            this->nco_r = r;
+            this->nco_i = i;
+        }
+        else
+        {
+            if (this->clk_i >= s1x14_0_0)
+            {
+                this->nco_r += this->clk_r;
+                if (this->nco_r >= s1x14_1_0)
+                {
+                    if (wave == triangle)
+                    {
+                        this->clk_i = -s1x14_1_0;
+                        this->nco_r = s1x14_1_0 - (this->nco_r - s1x14_1_0);
+                    }
+                    else
+                    {
+                        this->nco_r = this->nco_r - s1x14_1_0 - s1x14_1_0;
+                    }
+                }
+            }
+            else
+            {
+                this->nco_r -= this->clk_r;
+                if (this->nco_r <= -s1x14_1_0)
+                {
+                    if (wave == triangle)
+                    {
+                        this->clk_i = s1x14_1_0;
+                        this->nco_r = -s1x14_1_0 - (this->nco_r + s1x14_1_0);
+                    }
+                    else
+                    {
+                        this->nco_r = this->nco_r + s1x14_1_0 + s1x14_1_0;
+                    }
+                }
+            }
+        }
+    }
+
+    int r = chan[0].nco_r;
+    if (chan[0].wave == square)
+    {
+        if (r < s1x14_0_0)
+            pwm_set_chan_level(AUD_L_SLICE, AUD_L_CHAN, 0);
+        else
+            pwm_set_chan_level(AUD_L_SLICE, AUD_L_CHAN, AUD_PWM_WRAP + 1);
     }
     else
     {
-        norm = 0x7F;
-        s1x14 gain = int_to_s1x14(2) - (((int)r * (int)r + (int)i * (int)i) >> 14);
-        nco_r = muls1x14(r, gain);
-        nco_i = muls1x14(i, gain);
+        if (r < s1x14_0_0)
+            pwm_set_chan_level(AUD_L_SLICE, AUD_L_CHAN, AUD_PWM_CENTER - (-r >> AUD_SHIFT));
+        else
+            pwm_set_chan_level(AUD_L_SLICE, AUD_L_CHAN, AUD_PWM_CENTER + (r >> AUD_SHIFT));
+    }
+
+    static unsigned norm = 1;
+    if (!--norm)
+    {
+        norm = 1 << (AUD_SHIFT - 1);
+        for (unsigned idx = 0; idx < AUD_CHANNELS; idx++)
+        {
+            struct channel *this = &chan[idx];
+            enum waveform wave = this->wave;
+            if (wave <= sine)
+            {
+                int r = this->nco_r;
+                int i = this->nco_i;
+                s1x14 gain = s1x14_1_9r - ((r * r + i * i) >> 14);
+                this->nco_r = muls1x14(r, gain);
+                this->nco_i = muls1x14(i, gain);
+            }
+        }
     }
 }
 
@@ -74,24 +166,49 @@ void aud_init()
     config = pwm_get_default_config();
     pwm_config_set_wrap(&config, AUD_PWM_WRAP);
     pwm_init(AUD_L_SLICE, &config, true);
-    pwm_set_chan_level(AUD_L_SLICE, AUD_L_CHAN, AUD_PWM_WRAP / 2);
+    pwm_init(AUD_R_SLICE, &config, true);
+    pwm_set_chan_level(AUD_L_SLICE, AUD_L_CHAN, AUD_PWM_CENTER);
+    pwm_set_chan_level(AUD_R_SLICE, AUD_R_CHAN, AUD_PWM_CENTER);
 
     config = pwm_get_default_config();
-    pwm_config_set_wrap(&config, 240000u / (AUD_RATE / 1000));
+    pwm_config_set_wrap(&config, 120000u / (AUD_RATE / 1000));
     pwm_init(AUD_IRQ_SLICE, &config, true);
 
-    nco_r = int_to_s1x14(1);
-    nco_i = int_to_s1x14(0);
-
-    float freq = 440.0; // C4
-    // freq = 4186.009; // C8
+    float freq = 440.0; // A4
+    // freq = 32.7;    // C1
+    // freq = 65.41;    // C2
+    // freq = 4186.009;    // C8
     // freq = 2093.005; // C7
-
     float inc = M_PI * 2 * freq / AUD_RATE;
-    clk_r = float_to_s1x14(cosf(inc));
-    clk_i = float_to_s1x14(sinf(inc));
+    s1x14 clk_r = float_to_s1x14(cosf(inc));
+    s1x14 clk_i = float_to_s1x14(sinf(inc));
+
+    for (unsigned idx = 0; idx < AUD_CHANNELS; idx++)
+    {
+        pending[idx].nco_r = s1x14_1_0;
+        pending[idx].nco_i = s1x14_0_0;
+        pending[idx].clk_r = clk_r;
+        pending[idx].clk_i = clk_i;
+        pending[idx].wave = sine;
+        pending[idx].dirty = true;
+    }
+
+    // // saw
+    // pending[0].clk_r = float_to_s1x14(2 * freq / AUD_RATE);
+    // pending[0].clk_i = s1x14_1_0;  // up
+    // pending[0].clk_i = -s1x14_1_0; // down
+    // pending[0].wave = saw;
+
+    // // triangle
+    // pending[0].clk_r = float_to_s1x14(4 * freq / AUD_RATE);
+    // pending[0].clk_i = s1x14_1_0; // start up
+    // // pending[0].clk_i = -s1x14_1_0; // start down
+    // pending[0].wave = triangle;
+
+    // // square
+    // pending[0].wave = square;
 
     pwm_set_irq_enabled(AUD_IRQ_SLICE, true);
-    irq_set_exclusive_handler(PWM_IRQ_WRAP, audio_pwm_dma_irq_handler);
+    irq_set_exclusive_handler(PWM_IRQ_WRAP, audio_pwm_irq_handler);
     irq_set_enabled(PWM_IRQ_WRAP, true);
 }
--- src/dev/aud.h
+++ src/dev/aud.h
@@ -7,6 +7,12 @@
 #ifndef _AUD_H_
 #define _AUD_H_
 
+// Audio output pins, PWM stereo
+#define AUD_L_PIN 28
+#define AUD_R_PIN 27
+// IRQ pin used for PWM IRQ timer only, no IO
+#define AUD_IRQ_PIN 14
+
 void aud_init();
 void aud_task();
 void aud_reset();
