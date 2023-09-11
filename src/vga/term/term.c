9070b9503aa17bdf86ff88bfd77c17c752c98e39
rumbledethumps <16963588+rumbledethumps@users.noreply.github.com>
2023-09-11 15:17:27 -0700

    objectify term

--- src/vga/term/term.c
+++ src/vga/term/term.c
@@ -14,46 +14,62 @@
 #include "pico/scanvideo/composable_scanline.h"
 #include <stdio.h>
 
-// If you are extending this for use outside the Picocomputer,
-// CSI codes with multiple parameters will need a more complete
-// implementation first.
-
 #define TERM_WIDTH 80
 #define TERM_HEIGHT 32
 #define TERM_MEM_SIZE (TERM_WIDTH * TERM_HEIGHT * 2)
 #define TERM_WORD_WRAP 1
-static int term_x = 0, term_y = 0;
-static int term_y_offset = 0;
-static uint8_t term_color = 0x07;
-static uint32_t term_color_data[1024];
-static uint8_t term_memory[TERM_MEM_SIZE];
-static uint8_t *term_ptr = term_memory;
-static absolute_time_t term_timer = {0};
-static int32_t term_blink_state = 0;
-static ansi_state_t term_state = ansi_state_C0;
-static int term_csi_param;
 
-static void term_cursor_set_inv(bool inv)
+static uint32_t term_color_data[1024];
+
+typedef struct term_state
 {
-    if (term_blink_state == -1 || inv == term_blink_state || term_x >= TERM_WIDTH)
-        return;
-    term_ptr[1] = ((term_ptr[1] & 0x0F) << 4) | ((term_ptr[1] & 0xF0) >> 4);
-    term_blink_state = inv;
+    uint16_t x;
+    uint16_t y;
+    uint16_t y_offset;
+    uint8_t color;
+    uint8_t memory[TERM_MEM_SIZE];
+    uint8_t *ptr;
+    absolute_time_t timer;
+    int32_t blink_state;
+    ansi_state_t state;
+    int csi_param; // TODO make list
+} term_state_t;
+
+static term_state_t term_console;
+
+static void term_state_reset(term_state_t *term)
+{
+    term->x = 0;
+    term->y = 0;
+    term->y_offset = 0;
+    term->color = 0x07;
+    term->memory[TERM_MEM_SIZE];
+    term->ptr = term->memory;
+    term->blink_state = 0;
+    term->state = ansi_state_C0;
 }
 
-static void term_out_sgr(int param)
+static void term_cursor_set_inv(term_state_t *term, bool inv)
+{
+    if (term->blink_state == -1 || inv == term->blink_state || term->x >= TERM_WIDTH)
+        return;
+    term->ptr[1] = ((term->ptr[1] & 0x0F) << 4) | ((term->ptr[1] & 0xF0) >> 4);
+    term->blink_state = inv;
+}
+
+static void term_out_sgr(term_state_t *term, int param)
 {
     switch (param)
     {
     case -1:
     case 0: // reset
-        term_color = 0x07;
+        term->color = 0x07;
         break;
     case 1: // bold intensity
-        term_color = (term_color | 0x08);
+        term->color = (term->color | 0x08);
         break;
     case 22: // normal intensity
-        term_color = (term_color & 0xf7);
+        term->color = (term->color & 0xf7);
         break;
     case 30: // foreground color
     case 31:
@@ -63,7 +79,7 @@ static void term_out_sgr(int param)
     case 35:
     case 36:
     case 37:
-        term_color = (term_color & 0xf8) | (param - 30);
+        term->color = (term->color & 0xf8) | (param - 30);
         break;
     case 40: // background color
     case 41:
@@ -73,117 +89,117 @@ static void term_out_sgr(int param)
     case 45:
     case 46:
     case 47:
-        term_color = ((param - 40) << 4) | (term_color & 0x8f);
+        term->color = ((param - 40) << 4) | (term->color & 0x8f);
         break;
     }
 }
 
-static void term_out_ht()
+static void term_out_ht(term_state_t *term)
 {
-    if (term_x < TERM_WIDTH)
+    if (term->x < TERM_WIDTH)
     {
-        int xp = 8 - ((term_x + 8) & 7);
-        term_ptr += xp * 2;
-        term_x += xp;
+        int xp = 8 - ((term->x + 8) & 7);
+        term->ptr += xp * 2;
+        term->x += xp;
     }
 }
 
-static void term_out_lf()
+static void term_out_lf(term_state_t *term)
 {
-    term_ptr += TERM_WIDTH * 2;
-    if (term_ptr >= term_memory + TERM_MEM_SIZE)
+    term->ptr += TERM_WIDTH * 2;
+    if (term->ptr >= term->memory + TERM_MEM_SIZE)
     {
-        term_ptr -= TERM_MEM_SIZE;
+        term->ptr -= TERM_MEM_SIZE;
     }
 
-    if (++term_y == TERM_HEIGHT)
+    if (++term->y == TERM_HEIGHT)
     {
-        term_y = TERM_HEIGHT - 1;
-        if (++term_y_offset == TERM_HEIGHT)
+        term->y = TERM_HEIGHT - 1;
+        if (++term->y_offset == TERM_HEIGHT)
         {
-            term_y_offset = 0;
+            term->y_offset = 0;
         }
-        uint8_t *line_ptr = term_ptr - term_x * 2;
+        uint8_t *line_ptr = term->ptr - term->x * 2;
         for (size_t x = 0; x < TERM_WIDTH * 2; x += 2)
         {
             line_ptr[x] = ' ';
-            line_ptr[x + 1] = term_color;
+            line_ptr[x + 1] = term->color;
         }
     }
 }
 
-static void term_out_ff()
+static void term_out_ff(term_state_t *term)
 {
-    term_ptr -= term_x * 2;
-    term_ptr += (TERM_HEIGHT - term_y) * TERM_WIDTH * 2;
-    term_x = term_y = 0;
-    if (term_ptr >= term_memory + TERM_MEM_SIZE)
+    term->ptr -= term->x * 2;
+    term->ptr += (TERM_HEIGHT - term->y) * TERM_WIDTH * 2;
+    term->x = term->y = 0;
+    if (term->ptr >= term->memory + TERM_MEM_SIZE)
     {
-        term_ptr -= TERM_MEM_SIZE;
+        term->ptr -= TERM_MEM_SIZE;
     }
     for (size_t i = 0; i < TERM_MEM_SIZE; i += 2)
     {
-        term_memory[i] = ' ';
-        term_memory[i + 1] = term_color;
+        term->memory[i] = ' ';
+        term->memory[i + 1] = term->color;
     }
 }
 
-static void term_out_cr()
+static void term_out_cr(term_state_t *term)
 {
-    term_ptr -= term_x * 2;
-    term_x = 0;
+    term->ptr -= term->x * 2;
+    term->x = 0;
 }
 
-static void term_out_char(char ch)
+static void term_out_char(term_state_t *term, char ch)
 {
-    if (term_x == TERM_WIDTH)
+    if (term->x == TERM_WIDTH)
     {
         if (TERM_WORD_WRAP)
         {
-            term_out_cr();
-            term_out_lf();
+            term_out_cr(term);
+            term_out_lf(term);
         }
         else
         {
-            term_ptr -= 2;
-            term_x -= 1;
+            term->ptr -= 2;
+            term->x -= 1;
         }
     }
-    term_x++;
-    *term_ptr++ = ch;
-    *term_ptr++ = term_color;
+    term->x++;
+    *term->ptr++ = ch;
+    *term->ptr++ = term->color;
 }
 
 // Cursor forward
-static void term_out_cuf(int cols)
+static void term_out_cuf(term_state_t *term, int cols)
 {
-    if (cols > TERM_WIDTH - term_x)
-        cols = TERM_WIDTH - term_x;
-    term_ptr += cols * 2;
-    term_x += cols;
+    if (cols > TERM_WIDTH - term->x)
+        cols = TERM_WIDTH - term->x;
+    term->ptr += cols * 2;
+    term->x += cols;
 }
 
 // Cursor backward
-static void term_out_cub(int cols)
+static void term_out_cub(term_state_t *term, int cols)
 {
-    if (cols > term_x)
-        cols = term_x;
-    term_ptr -= cols * 2;
-    term_x -= cols;
+    if (cols > term->x)
+        cols = term->x;
+    term->ptr -= cols * 2;
+    term->x -= cols;
 }
 
 // Delete characters
-static void term_out_dch(int chars)
+static void term_out_dch(term_state_t *term, int chars)
 {
-    uint8_t *tp = term_ptr;
-    if (chars > TERM_WIDTH - term_x)
-        chars = TERM_WIDTH - term_x;
-    for (int i = term_x; i < TERM_WIDTH; i++)
+    uint8_t *tp = term->ptr;
+    if (chars > TERM_WIDTH - term->x)
+        chars = TERM_WIDTH - term->x;
+    for (int i = term->x; i < TERM_WIDTH; i++)
     {
         if (chars + i >= TERM_WIDTH)
         {
             tp[0] = ' ';
-            tp[1] = term_color;
+            tp[1] = term->color;
         }
         else
         {
@@ -194,47 +210,47 @@ static void term_out_dch(int chars)
     }
 }
 
-static void term_out_state_C0(char ch)
+static void term_out_state_C0(term_state_t *term, char ch)
 {
     if (ch == '\b')
-        term_out_cub(1);
+        term_out_cub(term, 1);
     else if (ch == '\t')
-        term_out_ht();
+        term_out_ht(term);
     else if (ch == '\n')
-        term_out_lf();
+        term_out_lf(term);
     else if (ch == '\f')
-        term_out_ff();
+        term_out_ff(term);
     else if (ch == '\r')
-        term_out_cr();
+        term_out_cr(term);
     else if (ch == '\33')
-        term_state = ansi_state_Fe;
+        term->state = ansi_state_Fe;
     else if (ch >= 32 && ch <= 255)
-        term_out_char(ch);
+        term_out_char(term, ch);
 }
 
-static void term_out_state_Fe(char ch)
+static void term_out_state_Fe(term_state_t *term, char ch)
 {
     if (ch == '[')
     {
-        term_state = ansi_state_CSI;
-        term_csi_param = -1;
+        term->state = ansi_state_CSI;
+        term->csi_param = -1;
     }
     else
-        term_state = ansi_state_C0;
+        term->state = ansi_state_C0;
 }
 
-static void term_out_state_CSI(char ch)
+static void term_out_state_CSI(term_state_t *term, char ch)
 {
     if (ch >= '0' && ch <= '9')
     {
-        if (term_csi_param < 0)
+        if (term->csi_param < 0)
         {
-            term_csi_param = ch - '0';
+            term->csi_param = ch - '0';
         }
         else
         {
-            term_csi_param *= 10;
-            term_csi_param += ch - '0';
+            term->csi_param *= 10;
+            term->csi_param += ch - '0';
         }
         return;
     }
@@ -242,52 +258,53 @@ static void term_out_state_CSI(char ch)
     {
         // all codes with multiple parameters
         // end up here where we assume SGR
-        term_out_sgr(term_csi_param);
-        term_csi_param = -1;
+        term_out_sgr(term, term->csi_param);
+        term->csi_param = -1;
         return;
     }
-    term_state = ansi_state_C0;
+    term->state = ansi_state_C0;
     if (ch == 'm')
     {
-        term_out_sgr(term_csi_param);
+        term_out_sgr(term, term->csi_param);
         return;
     }
     // Everything below defaults to 1
-    if (term_csi_param < 0)
-        term_csi_param = -term_csi_param;
+    if (term->csi_param < 0)
+        term->csi_param = -term->csi_param;
     if (ch == 'C')
-        term_out_cuf(term_csi_param);
+        term_out_cuf(term, term->csi_param);
     else if (ch == 'D')
-        term_out_cub(term_csi_param);
+        term_out_cub(term, term->csi_param);
     else if (ch == 'P')
-        term_out_dch(term_csi_param);
+        term_out_dch(term, term->csi_param);
 }
 
 static void term_out_chars(const char *buf, int length)
 {
     if (length)
     {
-        term_cursor_set_inv(false);
+        term_state_t *term = &term_console;
+        term_cursor_set_inv(term, false);
         for (int i = 0; i < length; i++)
         {
             char ch = buf[i];
             if (ch == ANSI_CANCEL)
-                term_state = ansi_state_C0;
+                term->state = ansi_state_C0;
             else
-                switch (term_state)
+                switch (term->state)
                 {
                 case ansi_state_C0:
-                    term_out_state_C0(ch);
+                    term_out_state_C0(term, ch);
                     break;
                 case ansi_state_Fe:
-                    term_out_state_Fe(ch);
+                    term_out_state_Fe(term, ch);
                     break;
                 case ansi_state_CSI:
-                    term_out_state_CSI(ch);
+                    term_out_state_CSI(term, ch);
                     break;
                 }
         }
-        term_timer = get_absolute_time();
+        term->timer = get_absolute_time();
     }
 }
 
@@ -300,6 +317,8 @@ static stdio_driver_t term_stdio = {
 
 void term_init(void)
 {
+    term_state_reset(&term_console);
+
     // become part of stdout
     stdio_set_driver_enabled(&term_stdio, true);
     for (int c = 0; c < 256; c++)
@@ -317,36 +336,38 @@ void term_init(void)
 
 void term_task(void)
 {
+    term_state_t *term = &term_console;
     absolute_time_t now = get_absolute_time();
-    if (absolute_time_diff_us(now, term_timer) < 0)
+    if (absolute_time_diff_us(now, term->timer) < 0)
     {
-        term_cursor_set_inv(!term_blink_state);
+        term_cursor_set_inv(term, !term->blink_state);
         // 0.3ms drift to avoid blinking cursor trearing
-        term_timer = delayed_by_us(now, 499700);
+        term->timer = delayed_by_us(now, 499700);
     }
 }
 
 void term_clear(void)
 {
     // reset state and clear screen
-    puts("\30\33[0m\f");
+    printf("\30\33[0m\f");
 }
 
 void term_render(struct scanvideo_scanline_buffer *dest, uint16_t height)
 {
     // renders 80 columns into 640 pixels with 16 fg/bg colors
     // requires PICO_SCANVIDEO_MAX_SCANLINE_BUFFER_WORDS=323
+    term_state_t *term = &term_console;
     int line = scanvideo_scanline_number(dest->scanline_id);
-    while (height <= term_y * 16)
+    while (height <= term->y * 16)
     {
         line += 16;
         height += 16;
     }
     const uint8_t *font_line = &font16[(line & 15) * 256];
-    line = line / 16 + term_y_offset;
+    line = line / 16 + term->y_offset;
     if (line >= TERM_HEIGHT)
         line -= TERM_HEIGHT;
-    uint8_t *term_ptr = term_memory + TERM_WIDTH * 2 * line;
+    uint8_t *term_ptr = term->memory + TERM_WIDTH * 2 * line;
     uint32_t *buf = dest->data;
     for (int i = 0; i < TERM_WIDTH * 2; i += 2)
     {
