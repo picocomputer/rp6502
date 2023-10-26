2de07fe32ce21850af2a3b80ad2ea41f75b652c8
rumbledethumps <16963588+rumbledethumps@users.noreply.github.com>
2023-10-25 23:22:29 -0700

    Revert "API for RTC issue #14 (#36)" (#37)
    This reverts commit for not having a license.

--- src/ria/main.c
+++ src/ria/main.c
@@ -7,7 +7,6 @@
 #include "main.h"
 #include "tusb.h"
 #include "api/api.h"
-#include "api/ntp.h"
 #include "api/oem.h"
 #include "api/rng.h"
 #include "api/std.h"
@@ -59,7 +58,6 @@ static void init(void)
     mou_init();
     rom_init();
     led_init();
-    ntp_init();
 
     // TinyUSB
     tuh_init(TUH_OPT_RHPORT);
@@ -172,15 +170,6 @@ bool main_api(uint8_t operation)
         break;
     case 0x04:
         rng_api_lrand();
-        break;
-    case 0x10:
-        ntp_api_get_res();
-        break;
-    case 0x11:
-        ntp_api_get_time();
-        break;
-    case 0x12:
-        ntp_api_set_time();
         break;
     case 0x14:
         std_api_open();
