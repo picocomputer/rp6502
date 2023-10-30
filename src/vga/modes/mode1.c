854b709198dc7682211bd1da7c1012cd2b3d5190
rumbledethumps <16963588+rumbledethumps@users.noreply.github.com>
2023-10-29 18:18:29 -0700

    started work

--- src/vga/modes/mode1.c
+++ src/vga/modes/mode1.c
@@ -7,26 +7,241 @@
 #include "modes/mode1.h"
 #include "pico/scanvideo.h"
 
+/*
+ * Copyright (c) 2023 Rumbledethumps
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#include "modes/mode3.h"
+#include "sys/vga.h"
+#include "sys/mem.h"
+#include "term/color.h"
+#include "term/font.h"
+#include "pico/scanvideo.h"
+#include <string.h>
+
 typedef struct
 {
-    int16_t xpos_px;
-    int16_t ypos_px;
+    bool x_wrap;
+    bool y_wrap;
+    int16_t x_pos_px;
+    int16_t y_pos_px;
     int16_t width_chars;
     int16_t height_chars;
     uint16_t xram_data_ptr;
-    uint16_t xram_color_ptr;
+    uint16_t xram_palette_ptr;
     uint16_t xram_font_ptr;
 } mode1_config_t;
 
 typedef struct
 {
-    uint8_t font_code;
+    uint8_t glyph_code;
+} mode1_1_data_t;
+
+typedef struct
+{
+    uint8_t glyph_code;
+    uint8_t fg_bg_index;
+} mode1_4r_data_t;
+
+typedef struct
+{
+    uint8_t glyph_code;
+    uint8_t bg_fg_index;
+} mode1_4_data_t;
+
+typedef struct
+{
+    uint8_t glyph_code;
+    uint16_t fg_index;
+    uint16_t bg_index;
+} mode1_8_data_t;
+
+typedef struct
+{
+    uint8_t glyph_code;
     uint8_t attributes;
     uint16_t fg_color;
     uint16_t bg_color;
 } mode1_16_data_t;
 
+static inline volatile const uint8_t *__attribute__((optimize("O1")))
+mode1_scanline_to_data(int16_t scanline_id, mode1_config_t *config, size_t cell_size, int16_t font_height, int16_t *row)
+{
+    *row = scanline_id - config->y_pos_px;
+    const int16_t height = config->height_chars * font_height;
+    if (config->y_wrap)
+    {
+        if (*row < 0)
+            *row += (-(*row + 1) / height + 1) * height;
+        if (*row >= height)
+            *row -= ((*row - height) / height + 1) * height;
+    }
+    if (*row < 0 || *row >= height || config->width_chars < 1 || height < 1)
+        return NULL;
+    const int32_t sizeof_row = (int32_t)config->width_chars * cell_size;
+    const int32_t sizeof_bitmap = (int32_t)config->height_chars * sizeof_row;
+    if (sizeof_bitmap > 0x10000 - config->xram_data_ptr)
+        return NULL;
+    return &xram[config->xram_data_ptr + *row / font_height * sizeof_row];
+}
+
+static inline volatile const uint16_t *__attribute__((optimize("O1")))
+mode1_get_palette(mode1_config_t *config, int16_t bpp)
+{
+    if (!(config->xram_palette_ptr & 1) &&
+        config->xram_palette_ptr <= 0x10000 - sizeof(uint16_t) * (2 ^ bpp))
+        return (uint16_t *)&xram[config->xram_palette_ptr];
+    if (bpp == 1)
+        return color_2;
+    return color_256;
+}
+
+static inline volatile const uint8_t *__attribute__((optimize("O1")))
+mode1_get_font(mode1_config_t *config, int16_t font_height)
+{
+    if (config->xram_font_ptr <= 0x10000 - 256 * font_height)
+        return &xram[config->xram_font_ptr];
+    if (font_height == 8)
+        return font8;
+    return font16;
+}
+
+static inline int16_t __attribute__((optimize("O1")))
+mode1_fill_cols(mode1_config_t *config, uint16_t **rgb, int16_t *col, int16_t *width)
+{
+    int16_t width_px = config->width_chars * 8;
+    if (*col < 0)
+    {
+        if (config->x_wrap)
+            *col += (-(*col + 1) / width_px + 1) * width_px;
+        else
+        {
+            uint16_t empty_cols = -*col;
+            if (empty_cols > *width)
+                empty_cols = *width;
+            memset(*rgb, 0, sizeof(uint16_t) * empty_cols);
+            *rgb += empty_cols;
+            *col += empty_cols;
+            *width -= empty_cols;
+            return 0;
+        }
+    }
+    if (*col >= width_px)
+    {
+        if (config->x_wrap)
+            *col -= ((*col - width_px) / width_px + 1) * width_px;
+        else
+        {
+            memset(*rgb, 0, sizeof(uint16_t) * (*width));
+            *width = 0;
+        }
+    }
+    int16_t fill_cols = *width;
+    if (fill_cols > width_px - *col)
+        fill_cols = width_px - *col;
+    *width -= fill_cols;
+    return fill_cols;
+}
+
+static bool __attribute__((optimize("O1")))
+mode1_render_1bpp_8x8(int16_t scanline_id, int16_t width, uint16_t *rgb, uint16_t config_ptr)
+{
+    if (config_ptr > 0x10000 - sizeof(mode1_config_t))
+        return false;
+    mode1_config_t *config = (void *)&xram[config_ptr];
+    int16_t row;
+    volatile const mode1_1_data_t *row_data =
+        (void *)mode1_scanline_to_data(scanline_id, config, sizeof(mode1_1_data_t), 8, &row);
+    if (!row_data)
+        return false;
+    volatile const uint16_t *palette = mode1_get_palette(config, 1);
+    volatile const uint8_t *font = mode1_get_font(config, 8) + 256 * (row & 7);
+    int16_t col = -config->x_pos_px;
+    while (width)
+    {
+        int16_t fill_cols = mode1_fill_cols(config, &rgb, &col, &width);
+        volatile const mode1_1_data_t *data = &row_data[col / 8];
+        uint8_t glyph = font[data->glyph_code];
+        int16_t part = 8 - (col & 7);
+        if (part > config->width_chars * 8 - col)
+            part = config->width_chars * 8 - col;
+        fill_cols -= part;
+        col += part;
+        switch (part)
+        {
+        case 8:
+            *rgb++ = palette[(glyph & 0x80) >> 7];
+        case 7:
+            *rgb++ = palette[(glyph & 0x40) >> 6];
+        case 6:
+            *rgb++ = palette[(glyph & 0x20) >> 5];
+        case 5:
+            *rgb++ = palette[(glyph & 0x10) >> 4];
+        case 4:
+            *rgb++ = palette[(glyph & 0x08) >> 3];
+        case 3:
+            *rgb++ = palette[(glyph & 0x04) >> 2];
+        case 2:
+            *rgb++ = palette[(glyph & 0x02) >> 1];
+        case 1:
+            *rgb++ = palette[glyph & 0x01];
+            glyph = font[(++data)->glyph_code];
+        }
+        col += fill_cols;
+        while (fill_cols > 7)
+        {
+            *rgb++ = palette[(glyph & 0x80) >> 7];
+            *rgb++ = palette[(glyph & 0x40) >> 6];
+            *rgb++ = palette[(glyph & 0x20) >> 5];
+            *rgb++ = palette[(glyph & 0x10) >> 4];
+            *rgb++ = palette[(glyph & 0x08) >> 3];
+            *rgb++ = palette[(glyph & 0x04) >> 2];
+            *rgb++ = palette[(glyph & 0x02) >> 1];
+            *rgb++ = palette[glyph & 0x01];
+            glyph = font[(++data)->glyph_code];
+            fill_cols -= 8;
+        }
+        if (fill_cols >= 1)
+            *rgb++ = palette[(glyph & 0x80) >> 7];
+        if (fill_cols >= 2)
+            *rgb++ = palette[(glyph & 0x40) >> 6];
+        if (fill_cols >= 3)
+            *rgb++ = palette[(glyph & 0x20) >> 5];
+        if (fill_cols >= 4)
+            *rgb++ = palette[(glyph & 0x10) >> 4];
+        if (fill_cols >= 5)
+            *rgb++ = palette[(glyph & 0x08) >> 3];
+        if (fill_cols >= 6)
+            *rgb++ = palette[(glyph & 0x04) >> 2];
+        if (fill_cols >= 7)
+            *rgb++ = palette[(glyph & 0x02) >> 1];
+    }
+    return true;
+}
+
 bool mode1_prog(uint16_t *xregs)
 {
-    return false;
+    const uint16_t attributes = xregs[2];
+    const uint16_t config_ptr = xregs[3];
+    const int16_t plane = xregs[4];
+    const int16_t scanline_begin = xregs[5];
+    const int16_t scanline_end = xregs[6];
+
+    if (config_ptr & 1 ||
+        config_ptr > 0x10000 - sizeof(mode1_config_t))
+        return false;
+
+    void *render_fn;
+    switch (attributes)
+    {
+    case 0:
+        render_fn = mode1_render_1bpp_8x8;
+        break;
+    default:
+        return false;
+    };
+
+    return vga_prog_fill(plane, scanline_begin, scanline_end, config_ptr, render_fn);
 }
