5e32871ce302f7cdfa51255127c5f7a0c630b9ba
rumbledethumps <16963588+rumbledethumps@users.noreply.github.com>
2023-12-30 18:04:13 -0800

    name cleanup

--- src/ria/aud/psg.c
+++ src/ria/aud/psg.c
@@ -15,7 +15,7 @@
 #define PSG_RATE 24000
 #define PSG_CHANNELS 8
 
-enum adsr_state
+enum psg_adsr_state
 {
     attack,
     decay,
@@ -25,7 +25,7 @@ enum adsr_state
 
 static volatile uint16_t psg_xaddr;
 
-static const uint32_t vol_table[] = {
+static const uint32_t psg_vol_table[] = {
     256 << 16,
     224 << 16,
     192 << 16,
@@ -45,7 +45,7 @@ static const uint32_t vol_table[] = {
 };
 
 // Same rates as the 6581 SID
-static const uint32_t attack_table[] = {
+static const uint32_t psg_attack_table[] = {
     (1 << 24) / (PSG_RATE / 1000 * 2),
     (1 << 24) / (PSG_RATE / 1000 * 8),
     (1 << 24) / (PSG_RATE / 1000 * 16),
@@ -65,7 +65,7 @@ static const uint32_t attack_table[] = {
 };
 
 // Same rates as the 6581 SID
-static const uint32_t decay_release_table[] = {
+static const uint32_t psg_decay_release_table[] = {
     (1 << 24) / (PSG_RATE / 1000 * 6),
     (1 << 24) / (PSG_RATE / 1000 * 24),
     (1 << 24) / (PSG_RATE / 1000 * 48),
@@ -84,28 +84,28 @@ static const uint32_t decay_release_table[] = {
     (1 << 24) / (PSG_RATE / 1000 * 24000),
 };
 
-struct channels
+struct psg_channel
 {
-    uint16_t duty;
     uint16_t freq;
-    uint8_t pan_gate;
+    uint16_t duty;
     uint8_t vol_attack;
     uint8_t vol_decay;
     uint8_t wave_release;
+    uint8_t pan_gate;
 };
 
 static struct
 {
-    uint8_t adsr;
     int8_t sample;
+    uint8_t adsr;
     uint32_t vol;
     uint32_t phase;
     uint32_t phase_inc;
     uint32_t noise1;
     uint32_t noise2;
-} channel_state[PSG_CHANNELS];
+} psg_channel_state[PSG_CHANNELS];
 
-static int8_t sine_table[256];
+static int8_t psg_sine_table[256];
 
 static void
     __attribute__((optimize("O1")))
@@ -115,7 +115,7 @@ static void
     pwm_clear_irq(AUD_IRQ_SLICE);
 
     // Check for valid xram address
-    struct channels *channels = (void *)&xram[psg_xaddr];
+    struct psg_channel *channels = (void *)&xram[psg_xaddr];
     if (channels == (void *)&xram[0xFFFF])
     {
         pwm_set_chan_level(AUD_L_SLICE, AUD_L_CHAN, AUD_PWM_CENTER);
@@ -128,8 +128,8 @@ static void
     int16_t sample_r = 0;
     for (unsigned i = 0; i < PSG_CHANNELS; i++)
     {
-        int8_t sample = channel_state[i].sample;
-        sample = ((int32_t)sample * (channel_state[i].vol >> 16)) >> 8;
+        int8_t sample = psg_channel_state[i].sample;
+        sample = ((int32_t)sample * (psg_channel_state[i].vol >> 16)) >> 8;
         int8_t pan = (int8_t)channels[i].pan_gate / 2;
         if (pan != -64)
         {
@@ -152,87 +152,87 @@ static void
     for (unsigned i = 0; i < PSG_CHANNELS; i++)
     {
         // Sample the raw waveform
-        channel_state[i].phase += channel_state[i].phase_inc;
-        uint32_t phase = channel_state[i].phase >> 16;
+        psg_channel_state[i].phase += psg_channel_state[i].phase_inc;
+        uint32_t phase = psg_channel_state[i].phase >> 16;
         uint32_t duty = channels[i].duty;
         switch (channels[i].wave_release >> 4)
         {
         case 0: // sine
             duty >>= 1;
             if (phase < 32768u - duty || phase >= 32768u + duty)
-                channel_state[i].sample = -127;
+                psg_channel_state[i].sample = -127;
             else
-                channel_state[i].sample = sine_table[phase >> 8];
+                psg_channel_state[i].sample = psg_sine_table[phase >> 8];
             break;
         case 1: // square
             if (phase > duty)
-                channel_state[i].sample = -127;
+                psg_channel_state[i].sample = -127;
             else
-                channel_state[i].sample = 127;
+                psg_channel_state[i].sample = 127;
             break;
         case 2: // sawtooth
             if (phase > duty)
-                channel_state[i].sample = -127;
+                psg_channel_state[i].sample = -127;
             else
-                channel_state[i].sample = 127 - (phase >> 8);
+                psg_channel_state[i].sample = 127 - (phase >> 8);
             break;
         case 3: // triangle
             duty >>= 1;
             if (phase < 32768u - duty || phase >= 32768u + duty)
-                channel_state[i].sample = -127;
+                psg_channel_state[i].sample = -127;
             else if (phase >= 32768)
-                channel_state[i].sample = 127 - (phase >> 7);
+                psg_channel_state[i].sample = 127 - (phase >> 7);
             else
-                channel_state[i].sample = (phase >> 7) - 128;
+                psg_channel_state[i].sample = (phase >> 7) - 128;
             break;
         case 4: // noise
             if ((phase) > duty)
-                channel_state[i].sample = -127;
+                psg_channel_state[i].sample = -127;
             else
             {
-                channel_state[i].noise1 ^= channel_state[i].noise2;
-                channel_state[i].sample = channel_state[i].noise2 & 0xFF;
-                channel_state[i].noise2 += channel_state[i].noise1;
+                psg_channel_state[i].noise1 ^= psg_channel_state[i].noise2;
+                psg_channel_state[i].sample = psg_channel_state[i].noise2 & 0xFF;
+                psg_channel_state[i].noise2 += psg_channel_state[i].noise1;
             }
             break;
         default:
-            channel_state[i].sample = 0;
+            psg_channel_state[i].sample = 0;
             break;
         }
 
         // Compute the ADSR envelope volume
-        if (!(channels[i].pan_gate & 0x01) && channel_state[i].adsr != release)
-            channel_state[i].adsr = release;
-        if ((channels[i].pan_gate & 0x01) && channel_state[i].adsr == release)
-            channel_state[i].adsr = attack;
-        switch (channel_state[i].adsr)
+        if (!(channels[i].pan_gate & 0x01) && psg_channel_state[i].adsr != release)
+            psg_channel_state[i].adsr = release;
+        if ((channels[i].pan_gate & 0x01) && psg_channel_state[i].adsr == release)
+            psg_channel_state[i].adsr = attack;
+        switch (psg_channel_state[i].adsr)
         {
         case attack:
-            channel_state[i].vol += attack_table[channels[i].vol_attack & 0xF];
-            if (channel_state[i].vol >= vol_table[channels[i].vol_attack >> 4])
+            psg_channel_state[i].vol += psg_attack_table[channels[i].vol_attack & 0xF];
+            if (psg_channel_state[i].vol >= psg_vol_table[channels[i].vol_attack >> 4])
             {
-                channel_state[i].vol = vol_table[channels[i].vol_attack >> 4];
-                channel_state[i].adsr = decay;
+                psg_channel_state[i].vol = psg_vol_table[channels[i].vol_attack >> 4];
+                psg_channel_state[i].adsr = decay;
             }
             break;
         case decay:
-            if (channel_state[i].vol <= decay_release_table[channels[i].vol_decay & 0xF])
-                channel_state[i].vol = 0;
+            if (psg_channel_state[i].vol <= psg_decay_release_table[channels[i].vol_decay & 0xF])
+                psg_channel_state[i].vol = 0;
             else
-                channel_state[i].vol -= decay_release_table[channels[i].vol_decay & 0xF];
-            if (channel_state[i].vol <= vol_table[channels[i].vol_decay >> 4])
+                psg_channel_state[i].vol -= psg_decay_release_table[channels[i].vol_decay & 0xF];
+            if (psg_channel_state[i].vol <= psg_vol_table[channels[i].vol_decay >> 4])
             {
-                channel_state[i].vol = vol_table[channels[i].vol_decay >> 4];
-                channel_state[i].adsr = sustain;
+                psg_channel_state[i].vol = psg_vol_table[channels[i].vol_decay >> 4];
+                psg_channel_state[i].adsr = sustain;
             }
             break;
         case sustain:
             break;
         case release:
-            if (channel_state[i].vol <= decay_release_table[channels[i].wave_release & 0xF])
-                channel_state[i].vol = 0;
+            if (psg_channel_state[i].vol <= psg_decay_release_table[channels[i].wave_release & 0xF])
+                psg_channel_state[i].vol = 0;
             else
-                channel_state[i].vol -= decay_release_table[channels[i].wave_release & 0xF];
+                psg_channel_state[i].vol -= psg_decay_release_table[channels[i].wave_release & 0xF];
             break;
         }
     }
@@ -244,14 +244,15 @@ static void psg_start(void)
     // https://www.musicdsp.org/en/latest/Synthesis/216-fast-whitenoise-generator.html
     for (unsigned i = 0; i < PSG_CHANNELS; i++)
     {
-        channel_state[i].noise1 = 0x67452301;
-        channel_state[i].noise2 = 0xEFCDAB89;
+        psg_channel_state[i].noise1 = 0x67452301;
+        psg_channel_state[i].noise2 = 0xEFCDAB89;
     }
 
     // Set up sine table
     for (unsigned i = 0; i < 256; i++)
-        sine_table[i] = cos(M_PI * 2.0 / 256 * i) * -127;
+        psg_sine_table[i] = cos(M_PI * 2.0 / 256 * i) * -127;
 
+    // Set the IRQ handler
     irq_set_exclusive_handler(PWM_IRQ_WRAP, psg_irq_handler);
 }
 
@@ -263,20 +264,20 @@ static void psg_reclock(uint32_t sys_clk_khz)
 static void psg_task(void)
 {
 
-    struct channels *channels = (void *)&xram[psg_xaddr];
+    struct psg_channel *channels = (void *)&xram[psg_xaddr];
     if (channels != (void *)&xram[0xFFFF])
     {
         // Perform slow computations on only 1 channel per task
-        static unsigned slow_chan = 0;
-        if (++slow_chan >= PSG_CHANNELS)
-            slow_chan = 0;
-        channel_state[slow_chan].phase_inc = ((double)UINT32_MAX + 1) * channels[slow_chan].freq / PSG_RATE;
+        static unsigned task_chan = 0;
+        if (++task_chan >= PSG_CHANNELS)
+            task_chan = 0;
+        psg_channel_state[task_chan].phase_inc = ((double)UINT32_MAX + 1) * channels[task_chan].freq / PSG_RATE;
     }
 }
 
 bool psg_xreg(uint16_t word)
 {
-    if (word > 0x10000 - PSG_CHANNELS * sizeof(struct channels))
+    if (word > 0x10000 - PSG_CHANNELS * sizeof(struct psg_channel))
     {
         psg_xaddr = 0xFFFF;
     }
