56d314f98a16fe3759be6207ff92e43ac007404e
rumbledethumps <16963588+rumbledethumps@users.noreply.github.com>
2025-05-27 20:05:35 -0700

    ntp timeout

--- src/ria/net/ntp.c
+++ src/ria/net/ntp.c
@@ -39,7 +39,7 @@ typedef enum
     ntp_state_dns_fail,
     ntp_state_request,
     ntp_state_request_wait,
-    ntp_state_request_fail,
+    ntp_state_request_timeout,
     ntp_state_set_time_fail,
     ntp_state_success,
     ntp_state_internal_error,
@@ -52,12 +52,14 @@ struct udp_pcb *ntp_pcb;
 bool ntp_success_at_least_once;
 int ntp_retry_retry_count;
 absolute_time_t ntp_retry_timer;
+absolute_time_t ntp_timeout_timer;
 
 // Be agressive 5 times then back off
 #define NTP_RETRY_RETRIES 5
 #define NTP_RETRY_RETRY_SECS 2
 #define NTP_RETRY_UNSET_SECS 60
 #define NTP_RETRY_REFRESH_SECS (24 * 3600)
+#define NTP_TIMEOUT_SECS 2
 
 static void ntp_retry(void)
 {
@@ -81,20 +83,21 @@ static void ntp_dns_found(const char *hostname, const ip_addr_t *ipaddr, void *a
     }
     else
     {
-        DBG("NTP DNS fail\n");
+        DBG("NET NTP DNS fail\n");
         ntp_retry();
         ntp_state = ntp_state_dns_fail;
     }
 }
 
-static void ntp_recv(void *arg, struct udp_pcb *pcb, struct pbuf *p, const ip_addr_t *addr, u16_t port)
+static void ntp_udp_recv(void *arg, struct udp_pcb *pcb, struct pbuf *p, const ip_addr_t *addr, u16_t port)
 {
     (void)arg;
     (void)pcb;
     uint8_t mode = pbuf_get_at(p, 0) & 0x7;
     uint8_t stratum = pbuf_get_at(p, 1);
 
-    if (ip_addr_cmp(addr, &ntp_server_address) && port == NTP_PORT && p->tot_len == NTP_MSG_LEN &&
+    if (ip_addr_cmp(addr, &ntp_server_address) &&
+        port == NTP_PORT && p->tot_len == NTP_MSG_LEN &&
         mode == 0x4 && stratum != 0)
     {
         uint8_t seconds_buf[4] = {0};
@@ -107,23 +110,17 @@ static void ntp_recv(void *arg, struct udp_pcb *pcb, struct pbuf *p, const ip_ad
         ts.tv_nsec = 0;
         if (aon_timer_set_time(&ts))
         {
-            DBG("NTP success\n");
+            DBG("NET NTP success\n");
             ntp_success_at_least_once = true;
             ntp_retry_timer = make_timeout_time_ms(NTP_RETRY_REFRESH_SECS * 1000);
             ntp_state = ntp_state_success;
         }
         else
         {
-            DBG("NTP set time fail\n");
+            DBG("NET NTP set time fail\n");
             ntp_retry();
             ntp_state = ntp_state_set_time_fail;
         }
-    }
-    else
-    {
-        DBG("NTP request failed\n");
-        ntp_retry();
-        ntp_state = ntp_state_request_fail;
     }
     pbuf_free(p);
 }
@@ -142,7 +139,7 @@ void ntp_task(void)
     switch (ntp_state)
     {
     case ntp_state_init:
-        DBG("NTP started\n");
+        DBG("NET NTP started\n");
         ntp_retry_retry_count = 0;
         ntp_pcb = udp_new_ip_type(IPADDR_TYPE_ANY);
         if (!ntp_pcb)
@@ -150,7 +147,7 @@ void ntp_task(void)
         else
         {
             ntp_state = ntp_state_dns;
-            udp_recv(ntp_pcb, ntp_recv, NULL);
+            udp_recv(ntp_pcb, ntp_udp_recv, NULL);
         }
         break;
     case ntp_state_dns:
@@ -167,28 +164,29 @@ void ntp_task(void)
         req[0] = 0x1b;
         udp_sendto(ntp_pcb, p, &ntp_server_address, NTP_PORT);
         pbuf_free(p);
+        ntp_timeout_timer = make_timeout_time_ms(NTP_TIMEOUT_SECS * 1000);
         ntp_state = ntp_state_request_wait;
         break;
-    case ntp_state_dns_wait:
-    case ntp_state_dns_fail:
     case ntp_state_request_wait:
-    case ntp_state_request_fail:
-    case ntp_state_set_time_fail:
+        if (absolute_time_diff_us(get_absolute_time(), ntp_timeout_timer) < 0)
+        {
+            DBG("NET NTP request timeout\n");
+            ntp_retry();
+            ntp_state = ntp_state_request_timeout;
+        }
+        break;
+    case ntp_state_dns_wait:
     case ntp_state_success:
     case ntp_state_internal_error:
         break;
-    }
-
-    switch (ntp_state)
-    {
     case ntp_state_dns_fail:
-    case ntp_state_request_fail:
+    case ntp_state_request_timeout:
     case ntp_state_set_time_fail:
         if (absolute_time_diff_us(get_absolute_time(), ntp_retry_timer) < 0)
+        {
             ntp_state = ntp_state_init;
-        break;
-    default:
-        break;
+            break;
+        }
     }
 }
 
@@ -211,8 +209,8 @@ void ntp_print_status(void)
     case ntp_state_request_wait:
         puts("requested");
         break;
-    case ntp_state_request_fail:
-        puts("failed");
+    case ntp_state_request_timeout:
+        puts("request timeout");
         break;
     case ntp_state_set_time_fail:
         puts("set time failure");
