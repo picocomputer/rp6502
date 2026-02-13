7a7528a3b48e75f7f12d657cf09f42b038d0d726
rumbledethumps <16963588+rumbledethumps@users.noreply.github.com>
2026-02-13 12:51:08 -0800

    led

--- src/ria/sys/led.c
+++ src/ria/sys/led.c
@@ -17,16 +17,14 @@ static inline void DBG(const char *fmt, ...) { (void)fmt; }
 
 #define LED_BLINK_TIME_MS 100
 
-bool led_state;
-bool led_blinking;
-absolute_time_t led_blink_timer;
+static bool led_state;
+static bool led_blinking;
+static absolute_time_t led_blink_timer;
 
 static void led_set(bool on)
 {
     led_state = on;
 #ifdef PICO_DEFAULT_LED_PIN
-    gpio_init(PICO_DEFAULT_LED_PIN);
-    gpio_set_dir(PICO_DEFAULT_LED_PIN, GPIO_OUT);
     gpio_put(PICO_DEFAULT_LED_PIN, on);
 #endif
 #ifdef RP6502_RIA_W
@@ -37,6 +35,10 @@ static void led_set(bool on)
 
 void led_init(void)
 {
+#ifdef PICO_DEFAULT_LED_PIN
+    gpio_init(PICO_DEFAULT_LED_PIN);
+    gpio_set_dir(PICO_DEFAULT_LED_PIN, GPIO_OUT);
+#endif
     led_set(true);
 }
 
