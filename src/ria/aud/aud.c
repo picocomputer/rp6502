56a6a6762b5f6f926fddc73c090e5d128d7e656a
rumbledethumps <16963588+rumbledethumps@users.noreply.github.com>
2026-03-17 14:35:48 -0700

    bel

--- src/ria/aud/aud.c
+++ src/ria/aud/aud.c
@@ -69,3 +69,54 @@ void aud_setup(void (*irq_fn)(void), uint32_t rate)
         irq_set_enabled(PWM_IRQ_WRAP, true);
     }
 }
+
+/* TODO
+
+Current audio system allows selection of none, opl, or psg.
+We are changing that to bel, opl, or psg.
+The new bel device is a single channel and the default.
+The opl and psg devices will mix in the bel devices.
+So the bel device will need to support variable sample rates
+and default to 24000 Hz.
+
+The bel device is not mapped into xram. It will be used from C.
+The bel device is similar to the PSG except it has only one mono channel
+and will be used from C instead of xram.
+
+The bel api will be:
+
+typedef struct
+{
+    uint16_t freq;
+    uint8_t duty;
+    uint8_t vol_attack;
+    uint8_t vol_decay;
+    uint8_t wave_release;
+    uint16_t restrike_ms;
+    uint16_t release_ms;
+    uint16_t end_ms;
+} ria_bel_t;
+
+aud_bel_add(ria_bel_t *sound)
+
+Up to 8 sounds can be queued.
+
+In some cases, we'll play a few notes in sequence
+where end_ms is after release_ms and restrike_ms is 0.
+The generator will begin in attack and be released in
+release_ms with the next note starting end_ms since the start.
+
+In other cases, we'll be "ringing a bell". If restrike_ms is >0
+and that amount of time has elapsed and the next sound also has
+restrike_ms >0 then the generator will be immediately returned to
+attack mode regardless of release_ms or end_ms. If the next sound
+is restrike_ms==0, or there is no next sound, then release_ms and
+end_ms work normally.
+
+Move the psh shared constants into bel.h.
+
+Make a few "Bell" sounds for me to try out. Small bells like old
+typewriters and teletypes have are what I want. Put them in Pico flash.
+
+
+*/
