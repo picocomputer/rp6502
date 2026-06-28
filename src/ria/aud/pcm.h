f5c98b0404c5a91725c4a6a6b128254ae521e668
WojciechGw <wojciech.gwiozdzik@gmail.com>
2026-06-28 06:46:09 +0200

    Add PCM audio driver

---
+++ src/ria/aud/pcm.h
@@ -0,0 +1,19 @@
+/*
+ * Copyright (c) 2026 Rumbledethumps
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#ifndef _RIA_AUD_PCM_H_
+#define _RIA_AUD_PCM_H_
+
+/* PCM audio playback - 16-bit stereo 44100 Hz ring buffer in XRAM.
+ */
+
+#include <stddef.h>
+#include <stdint.h>
+#include <stdbool.h>
+
+bool pcm_xreg(uint16_t word);
+
+#endif /* _RIA_AUD_PCM_H_ */
